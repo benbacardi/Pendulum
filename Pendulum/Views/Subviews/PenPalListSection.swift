99f9c28a77f8a97edca7fd62e30f3e875db8b351
Ben Cardy <ben.cardy@ocado.com>
2022-11-05 11:57:50 +0000

    Missed file

---
+++ Pendulum/Views/Subviews/PenPalListSection.swift
@@ -0,0 +1,42 @@
+//
+//  PenPalListSection.swift
+//  Pendulum
+//
+//  Created by Ben Cardy on 05/11/2022.
+//
+
+import SwiftUI
+
+struct PenPalListSection: View {
+    
+    // MARK: Parameters
+    let type: EventType
+    let penpals: [PenPal]
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: type.icon)
+                    .font(.headline)
+                Text(type.phrase)
+                    .font(.headline)
+                    .fullWidth()
+            }
+            ForEach(penpals) { penpal in
+                Text(penpal.fullName)
+                    .fullWidth()
+            }
+        }
+        .padding()
+    }
+}
+
+struct PenPalListSection_Previews: PreviewProvider {
+    static var previews: some View {
+        PenPalListSection(type: .written, penpals: [
+            PenPal(id: "1", givenName: "Ben", familyName: "Cardy", image: nil),
+            PenPal(id: "2", givenName: "Alex", familyName: "Faber", image: nil),
+            PenPal(id: "3", givenName: "Madi", familyName: "Van Houten", image: nil)
+        ])
+    }
+}
