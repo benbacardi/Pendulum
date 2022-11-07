1e6fd10f8ba3077c1fd4535a90d3c7f927e1f2a5
Alex Faber <alexcfaber@gmail.com>
2022-11-07 19:58:18 +0100

    OPT: Applying the one thing I seem to apply everywhere (fixing the tabbar)

--- Pendulum/PendulumApp.swift
+++ Pendulum/PendulumApp.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 @main
 struct PendulumApp: App {
+    init() {
+            // This fixes a bug / feature introduced in iOS 15
+            // where the TabBar in SwiftUI is transparent by default.
+            let appearance = UITabBarAppearance()
+            appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+            // Scrolling behind
+            UITabBar.appearance().standardAppearance = appearance
+            // When scrolled all the way up
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
