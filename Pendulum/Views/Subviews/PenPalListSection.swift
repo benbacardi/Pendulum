13db463d4f66bd00c00f741ed48e94255e6d9879
Ben Cardy <ben.cardy@ocado.com>
2022-11-09 12:31:44 +0000

    Improve list yet again

--- Pendulum/Views/Subviews/PenPalListSection.swift
+++ Pendulum/Views/Subviews/PenPalListSection.swift
@@ -32,107 +32,99 @@ struct PenPalListSection: View {
     var body: some View {
         VStack {
             HStack {
-                HStack(spacing: 0) {
+                ZStack {
                     Rectangle()
                         .fill(type.color)
-                        .frame(width: 10, height: iconWidth)
-                    ZStack {
-                        Rectangle()
-                            .fill(type.color)
-                            .frame(width: iconWidth * 1.2, height: iconWidth)
-                            .cornerRadius(100, corners: [.topRight, .bottomRight])
-                        Image(systemName: type.phraseIcon)
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .background(GeometryReader { geo in
-                                Color.clear.preference(key: PenPalListIconWidthPreferenceKey.self, value: geo.size.width)
-                            })
-                    }
+                        .frame(width: iconWidth * 1.2, height: iconWidth * 1.2)
+                        .cornerRadius(100, corners: .allCorners)
+                    Image(systemName: type.phraseIcon)
+                        .font(.caption)
+                        .bold()
+                        .foregroundColor(.white)
+                        .background(GeometryReader { geo in
+                            Color.clear.preference(key: PenPalListIconWidthPreferenceKey.self, value: geo.size.width)
+                        })
                 }
                 Text(type.phrase)
                     .fullWidth()
-                Spacer()
             }
-            VStack {
-                ForEach(penpals) { penpal in
-                    NavigationLink(destination: PenPalView(penpal: penpal)) {
-                        GroupBox {
-                            VStack {
-                                HStack {
-                                    if let image = penpal.displayImage {
-                                        image
-                                            .clipShape(Circle())
-                                            .frame(width: 40, height: 40)
-                                    } else {
-                                        ZStack {
-                                            Circle()
-                                                .fill(.gray)
-                                            Text(penpal.initials)
-                                                .font(.system(.headline, design: .rounded))
-                                                .foregroundColor(.white)
-                                        }
+            ForEach(penpals) { penpal in
+                NavigationLink(destination: PenPalView(penpal: penpal)) {
+                    GroupBox {
+                        VStack {
+                            HStack {
+                                if let image = penpal.displayImage {
+                                    image
+                                        .clipShape(Circle())
                                         .frame(width: 40, height: 40)
+                                } else {
+                                    ZStack {
+                                        Circle()
+                                            .fill(.gray)
+                                        Text(penpal.initials)
+                                            .font(.system(.headline, design: .rounded))
+                                            .foregroundColor(.white)
                                     }
-                                    VStack {
-                                        Text(penpal.name)
-                                            .font(.headline)
+                                    .frame(width: 40, height: 40)
+                                }
+                                VStack {
+                                    Text(penpal.name)
+                                        .font(.headline)
+                                        .fullWidth()
+                                    if penpal.lastEventDate != nil && penpal.lastEventType != .archived {
+                                        self.dateText(for: penpal)
+                                            .font(.caption)
                                             .fullWidth()
-                                        if penpal.lastEventDate != nil && penpal.lastEventType != .archived {
-                                            self.dateText(for: penpal)
-                                                .font(.caption)
-                                                .fullWidth()
-                                        }
                                     }
-                                    Image(systemName: "chevron.right")
-                                        .foregroundColor(.secondary)
                                 }
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.secondary)
                             }
-                            .foregroundColor(.primary)
                         }
-                        .contextMenu {
-                            ForEach(EventType.actionableCases, id: \.self) { eventType in
-                                Button(action: {
-                                    self.currentPenPal = penpal
-                                    self.presentAddEventSheetForType = eventType
-                                }) {
-                                    Label(eventType.actionableText, systemImage: eventType.icon)
-                                }
-                            }
-                            Divider()
+                        .foregroundColor(.primary)
+                    }
+                    .contextMenu {
+                        ForEach(EventType.actionableCases, id: \.self) { eventType in
                             Button(action: {
-                                Task {
-                                    await penpal.archive()
-                                }
-                            }) {
-                                Label("Archive", systemImage: "archivebox")
-                            }
-                            Button(role: .destructive, action: {
                                 self.currentPenPal = penpal
-                                self.showDeleteAlert = true
+                                self.presentAddEventSheetForType = eventType
                             }) {
-                                Label("Delete", systemImage: "trash")
+                                Label(eventType.actionableText, systemImage: eventType.icon)
                             }
                         }
-                        .confirmationDialog("Are you sure?", isPresented: $showDeleteAlert, titleVisibility: .visible, presenting: currentPenPal) { penpal in
-                            Button("Delete \(penpal.name)", role: .destructive) {
-                                Task {
-                                    await penpal.delete()
-                                }
+                        Divider()
+                        Button(action: {
+                            Task {
+                                await penpal.archive()
+                            }
+                        }) {
+                            Label("Archive", systemImage: "archivebox")
+                        }
+                        Button(role: .destructive, action: {
+                            self.currentPenPal = penpal
+                            self.showDeleteAlert = true
+                        }) {
+                            Label("Delete", systemImage: "trash")
+                        }
+                    }
+                    .confirmationDialog("Are you sure?", isPresented: $showDeleteAlert, titleVisibility: .visible, presenting: currentPenPal) { penpal in
+                        Button("Delete \(penpal.name)", role: .destructive) {
+                            Task {
+                                await penpal.delete()
                             }
                         }
                     }
                 }
             }
-            .sheet(item: $presentAddEventSheetForType) { eventType in
-                if let penpal = self.currentPenPal {
-                    AddEventSheet(penpal: penpal, event: nil, eventType: eventType) { newEvent, newEventType in
-                        self.presentAddEventSheetForType = nil
-                    }
-                }
-            }
-            .padding(.horizontal)
         }
-        .padding(.top)
+        .sheet(item: $presentAddEventSheetForType) { eventType in
+            if let penpal = self.currentPenPal {
+                AddEventSheet(penpal: penpal, event: nil, eventType: eventType) { newEvent, newEventType in
+                    self.presentAddEventSheetForType = nil
+                }
+            }
+        }
+        .padding([.horizontal, .top])
         .opacity(type == .archived ? 0.5 : 1)
     }
 }
