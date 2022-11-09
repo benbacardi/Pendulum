83736d8e35a5f412850d1e6189d4bd6d691bb359
Ben Cardy <ben.cardy@ocado.com>
2022-11-09 21:11:20 +0000

    Bump version

--- Pendulum/Views/ContentView.swift
+++ Pendulum/Views/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var selectedTab: Int = Tab.settings.rawValue
+    @State private var selectedTab: Int = Tab.penPalList.rawValue
     
     var body: some View {
         TabView(selection: $selectedTab) {
