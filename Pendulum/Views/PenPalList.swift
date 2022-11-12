ddc0f73376c720b3f92e9099ef09af09cfcbc944
Ben Cardy <ben.cardy@ocado.com>
2022-11-12 20:43:11 +0000

    Try using sections and list

--- Pendulum/Views/PenPalList.swift
+++ Pendulum/Views/PenPalList.swift
@@ -15,6 +15,14 @@ struct PresentAddEventSheet: Identifiable {
     let eventType: EventType
 }
 
+struct PenPalSection: Identifiable, Hashable {
+    let eventType: EventType
+    let penpals: [PenPal]
+    var id: Int { eventType.rawValue }
+}
+
+
+
 struct PenPalList: View {
     
     // MARK: Environment
@@ -29,6 +37,120 @@ struct PenPalList: View {
     @State private var presentingStationerySheet: Bool = false
     @State private var presentAddEventSheetForType: PresentAddEventSheet? = nil
     @State private var columnVisibility: NavigationSplitViewVisibility = .all
+    @State private var currentPenPal: PenPal? = nil
+    @State private var showDeleteAlert = false
+    
+    func dateText(for penpal: PenPal) -> Text {
+        if let date = penpal.lastEventDate {
+            return Text("\(penpal.lastEventType.datePrefix) \(Calendar.current.verboseNumberOfDaysBetween(date, and: Date()))")
+        } else {
+            return Text("")
+        }
+    }
+    
+    @ViewBuilder
+    func sectionHeader(for type: EventType) -> some View {
+        HStack {
+            ZStack {
+                Rectangle()
+                    .fill(type.color)
+                    .frame(width: iconWidth * 1.2, height: iconWidth * 1.2)
+                    .cornerRadius(100, corners: .allCorners)
+                Image(systemName: type.phraseIcon)
+                    .font(.caption)
+                    .bold()
+                    .foregroundColor(.white)
+                    .background(GeometryReader { geo in
+                        Color.clear.preference(key: PenPalListIconWidthPreferenceKey.self, value: geo.size.width)
+                    })
+            }
+            Text(type.phrase)
+                .fullWidth()
+                .font(.body)
+                .foregroundColor(.primary)
+        }
+    }
+    
+    @ViewBuilder
+    func penPalNavigationLink(for penpal: PenPal) -> some View {
+        NavigationLink(destination: PenPalView(penpal: penpal)) {
+            GroupBox {
+                VStack {
+                    HStack {
+                        if let image = penpal.displayImage {
+                            image
+                                .clipShape(Circle())
+                                .frame(width: 40, height: 40)
+                        } else {
+                            ZStack {
+                                Circle()
+                                    .fill(.gray)
+                                Text(penpal.initials)
+                                    .font(.system(.headline, design: .rounded))
+                                    .foregroundColor(.white)
+                            }
+                            .frame(width: 40, height: 40)
+                        }
+                        VStack {
+                            Text(penpal.name)
+                                .font(.headline)
+                                .fullWidth()
+                            if penpal.lastEventDate != nil && penpal.lastEventType != .archived {
+                                self.dateText(for: penpal)
+                                    .font(.caption)
+                                    .fullWidth()
+                            }
+                        }
+                    }
+                }
+                .foregroundColor(.primary)
+            }
+            .contextMenu {
+                ForEach(EventType.actionableCases, id: \.self) { eventType in
+                    Button(action: {
+                        if eventType.presentFullNotesSheetByDefault && !UserDefaults.shared.enableQuickEntry {
+                            self.presentAddEventSheetForType = PresentAddEventSheet(penpal: penpal, eventType: eventType)
+                        } else {
+                            Task {
+                                await penpal.addEvent(ofType: eventType)
+                            }
+                        }
+                    }) {
+                        Label(eventType.actionableText, systemImage: eventType.icon)
+                    }
+                }
+                Divider()
+                Button(action: {
+                    Task {
+                        if penpal.lastEventType != .archived {
+                            await penpal.archive()
+                        } else {
+                            await penpal.updateLastEventType()
+                        }
+                    }
+                }) {
+                    if penpal.lastEventType != .archived {
+                        Label("Archive", systemImage: "archivebox")
+                    } else {
+                        Label("Unarchive", systemImage: "archivebox")
+                    }
+                }
+                Button(role: .destructive, action: {
+                    self.currentPenPal = penpal
+                    self.showDeleteAlert = true
+                }) {
+                    Label("Delete", systemImage: "trash")
+                }
+            }
+            .confirmationDialog("Are you sure?", isPresented: $showDeleteAlert, titleVisibility: .visible, presenting: currentPenPal) { penpal in
+                Button("Delete \(penpal.name)", role: .destructive) {
+                    Task {
+                        await penpal.delete()
+                    }
+                }
+            }
+        }
+    }
     
     var body: some View {
         NavigationSplitView(columnVisibility: $columnVisibility) {
@@ -58,14 +180,28 @@ struct PenPalList: View {
                     }
                     .padding()
                 } else {
-                    ScrollView {
-                        ForEach(EventType.allCases, id: \.self) { eventType in
-                            if let penpals = penPalListController.groupedPenPals[eventType] {
-                                PenPalListSection(type: eventType, penpals: penpals, iconWidth: $iconWidth, presentAddEventSheetForType: $presentAddEventSheetForType)
+                    List {
+                        ForEach(penPalListController.penPalSections, id: \.eventType.rawValue) { section in
+                            Section(header: sectionHeader(for: section.eventType)) {
+                                ForEach(section.penpals, id: \.id) { penpal in
+                                    penPalNavigationLink(for: penpal)
+                                }
                             }
+//                            .headerProminence(.increased)
                         }
-                        Spacer()
+                        .listRowSeparator(.hidden)
                     }
+                    .listStyle(.plain)
+//                    ScrollView {
+//                        LazyVStack {
+//                            ForEach(EventType.allCases, id: \.self.rawValue) { eventType in
+//                                if let penpals = penPalListController.groupedPenPals[eventType] {
+//                                    PenPalListSection(type: eventType, penpals: penpals, iconWidth: $iconWidth, presentAddEventSheetForType: $presentAddEventSheetForType)
+//                                }
+//                            }
+//                            Spacer()
+//                        }
+//                    }
                 }
             }
             .navigationTitle("Pen Pals")
