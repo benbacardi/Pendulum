dad47f421125440554d0afbcbfbc1e798a264b2c
Ben Cardy <me@bencardy.co.uk>
2023-03-27 16:07:04 +0100

    Take pictures from camera, save to roll, use thumbnail version

--- Pendulum/Data/EventPhoto+Extensions.swift
+++ Pendulum/Data/EventPhoto+Extensions.swift
@@ -18,8 +18,22 @@ extension EventPhoto {
         return eventPhoto
     }
     
+    static func from(_ image: UIImage) -> EventPhoto {
+        let eventPhoto = EventPhoto(context: PersistenceController.shared.container.viewContext)
+        eventPhoto.id = UUID()
+        eventPhoto.data = image.jpegData(compressionQuality: 1.0) ?? Data()
+        eventPhoto.thumbnailData = image.resize(targetSize: CGSize(width: 200, height: 200))?.jpegData(compressionQuality: 1.0) ?? Data()
+        eventPhoto.dateAdded = Date()
+        return eventPhoto
+    }
+    
     func image() -> Image? {
         guard let data = self.data, let uiImage = UIImage(data: data) else { return nil }
+        return Image(uiImage: uiImage)
+    }
+    
+    func thumbnail() -> Image? {
+        guard let thumbnailData = self.thumbnailData, let uiImage = UIImage(data: thumbnailData) else { return nil }
         return Image(uiImage: uiImage)
     }
 }
